de63ab13c78f82f9fbf89033305a589d5b4deb0b
Naval <navalnovel@gmail.com>
2016-06-18 19:26:32 +0530

    updates for new version of archiejs which uses promises instead of callbacks

--- test/dbwrapper.js
+++ test/dbwrapper.js
@@ -5,9 +5,10 @@ require('./../');
 var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
-var async = require('async');
-var resolve = require('path').resolve;
-var basedir = resolve(__dirname, '..');
+var promisify = require("es6-promisify");
+
+var pathResolve = require('path').resolve;
+var basedir = pathResolve(__dirname, '..');
 
 var MongoEnhancer = require('./../lib/mongo_enhancer');
 var mongoEnhancer;
@@ -35,50 +36,41 @@ describe('Mongo Enhancer Testcases:', function(){
     });
 
     it('#reads/writes to mongodb', function(done){
-        this.timeout(10000);
-        var db;
-        async.waterfall([
-            function(cb){
+        var db = {};
+
+        Promise.resolve()
+            .then(() => {
                 mongoEnhancer.resolveConfig(configTest, basedir);
                 configTest.provides.length.should.equal(1);
                 configTest.enhancers.should.have.property('Person');
-                cb();
-            },
-            function(cb){
-                mongoEnhancer.setupPlugin(configTest, {},
-                    function(err, services){
-                        db = services;
-                        db.should.have.property('Person');
-                        cb();
-                    }
-                );
-            },
-            // write data
-            function(cb){
+            })
+            .then(() => mongoEnhancer.setupPlugin.call(mongoEnhancer, configTest))
+            .then((_db) => { db = _db; })
+            .then(() => {
+
+                // write data
                 var person = new db.Person({
                     name: 'dolly',
                     age: 2
                 });
-                person.save(function(err, data) {
-                    expect(err).to.not.exist;
-                    expect(data._id).to.exist;
-                    data.name.should.equal('dolly');
-                    data.age.should.equal(2);
-                    return cb();
-                });
-            },
-            // read data
-            function(cb){
-                db.Person.findOne({name: 'dolly'},
-                function(err, data){
-                    expect(err).to.not.exist;
-                    expect(data._id).to.exist;
-                    data.name.should.equal('dolly');
-                    data.age.should.equal(2);
-                    return cb();
-                });
-            }
-        ], done);
+                return promisify(person.save, person)();
+            })
+            .then((data) => {
+                expect(data._id).to.exist;
+                data.name.should.equal('dolly');
+                data.age.should.equal(2);
+            })
+            .then(() => {
+                // read data
+                return promisify(db.Person.findOne, db.Person)({name: 'dolly'});
+            })
+            .then((data) => {
+                expect(data._id).to.exist;
+                data.name.should.equal('dolly');
+                data.age.should.equal(2);
+            })
+            .then(done);
+
     });
 
 });
