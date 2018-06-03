5060ad0c837069b0b798b1217a87e5b59317a70f
Naval Saini <naval_saini@mckinsey.com>
2018-06-03 13:58:57 +0530

    more changes for async await

--- test/dbwrapper.js
+++ test/dbwrapper.js
@@ -31,40 +31,28 @@ describe('Mongo Enhancer Testcases:', function(){
         mongoEnhancer = new MongoEnhancer();
     });
 
-    afterEach(function(){
-        mongoEnhancer.closeClient();
+    afterEach(async function (){
+        await mongoEnhancer.closeClient();
     });
 
-    it('#reads/writes to mongodb', function(done){
-        var db = {};
+    it('#reads/writes to mongodb', async function(){
+        mongoEnhancer.resolveConfig(configTest, basedir);
+        configTest.provides.length.should.equal(1);
+        configTest.enhancers.should.have.property('Person');
+        
+        var dbService = await mongoEnhancer.setupPlugin(configTest);
 
-        Promise.resolve()
-            .then(() => {
-                mongoEnhancer.resolveConfig(configTest, basedir);
-                configTest.provides.length.should.equal(1);
-                configTest.enhancers.should.have.property('Person');
-            })
-            .then(() => mongoEnhancer.setupPlugin.call(mongoEnhancer, configTest))
-            .then((_db) => { db = _db; })
-            .then(() => {
-                // write data
-                var person = new db.Person({
-                    name: 'dolly',
-                    age: 2
-                });
-                return promisify(person.save, person)();
-            })
-            .then(() => {
-                // read data
-                return promisify(db.Person.findOne, db.Person)({name: 'dolly'});
-            })
-            .then((data) => {
-                expect(data._id).to.exist;
-                data.name.should.equal('dolly');
-                data.age.should.equal(2);
-            })
-            .then(done);
+        // write data
+        var person = new dbService.Person({
+            name: 'dolly',
+            age: 2
+        });
+        await person.save();
 
+        var data = await dbService.Person.findOne({name: 'dolly'}).exec();
+        expect(data._id).to.exist;
+        data.name.should.equal('dolly');
+        data.age.should.equal(2);
     });
 
 });
